f9227c7029409234e91e7fe9f4c71993dfbf6b8c
ponyoMe <esilwasborninthiscorne@gmail.com>
2025-08-27 14:01:55 +0500

    solution for two pointers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the length of the array: ");
+        int n = scan.nextInt();
+        int[] array = new int[n];
+        System.out.print("Enter the numbers for the array: ");
+        for(int i = 0; i<n; i++){
+            array[i] = scan.nextInt();
+        }
+
+        int l = 0;
+        for(int r=1; r<n; r++){
+            if(array[l] != array[r]){
+                l++;
+                array[l] = array[r];
+            }
+        }
+
+        for(int i = 0; i<=l; i++){
+            System.out.print(array[i]+" ");
+        }
     }
 }
